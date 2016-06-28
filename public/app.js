a9505ad531e5148b45c3460ff96d9d136eab2ef0
Chris Clark <chrisclark1979@gmail.com>
2016-06-28 12:10:39 -0700

    MVP complete

---
+++ public/app.js
@@ -0,0 +1,140 @@
+var app = angular.module('filmLocations', ['ngMaterial','autofill-directive'])
+
+app.controller('DataCtrl', function($scope, $rootScope, $http) {
+    
+    (function loadTags(){
+        //Populates autocomplete fields by makeing an initial call to SFDATA api, 
+        //parses through movie titles and eliminates duplicate entries
+
+        $http.get("https://data.sfgov.org/resource/wwmu-gmzc.json?")
+        .then(function(res){
+            var movies = [res.data[0].title], len = res.data.length;
+            for(var i = 1; i < len; i++){
+                //excessive && statements below due to some duplicate movie titles not being listed sequentially.
+                if(res.data[i].title !== res.data[i-1].title && res.data[i].title !== res.data[i-2].title && res.data[i].title !== res.data[i-3].title){
+                    movies.push(res.data[i].title);
+                }  
+            }
+            $scope.filmNames = movies;
+            console.log(movies.length + " movies available", $scope.filmNames);
+            
+            //alternative JQuery autocomplete
+           // $(function() {
+           //      $( "#text" ).autocomplete({
+           //        source: $scope.filmNames
+           //      });
+           //  });
+
+            new autoComplete({
+                selector: 'input[id="text"]',
+                minChars: 1,
+                source: function(term, suggest){
+                    term = term.toLowerCase();
+                    var choices = $scope.filmNames;
+                    var matches = [];
+                    for (i=0; i<choices.length; i++)
+                        if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
+                    suggest(matches);
+                }
+            });
+
+        })
+    })()
+
+    $scope.searchTitles = function () {
+        //on form submission, queries SFDATA for all locations for submitted title
+        //upon receipt, assigns $rootScope variable to the returned array and emits
+        //an event which the map controller is listening for
+
+        $http.get("https://data.sfgov.org/resource/wwmu-gmzc.json?title="+$scope.title)
+        .then(function(res){ 
+            var places = [], len = res.data.length;
+            for(var i = 0; i < len; i++){
+                places.push(res.data[i].locations);
+            }
+            $rootScope.locations = places;
+            console.log($scope.title + " was filmed in these locations: ", $rootScope.locations);
+            $rootScope.$emit('rootScope:emit', 'MapCtrl, would you mind changing the markers? Thanks.');
+        });
+    };  
+
+})
+
+
+app.controller('MapCtrl', function($scope, $rootScope, $http) {
+
+    var sfCenter = {lat: 37.78, lng: -122.42};
+    var mapOptions = {
+        center: sfCenter,
+        zoom: 11,
+        minZoom: 11,
+        maxZoom: 16
+    };
+
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    $scope.markers = [];
+
+    var infoWindow = new google.maps.InfoWindow();
+    
+    var createMarker = function (info){
+        var marker = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(info.lat, info.long),
+            title: info.name
+        });
+        //marker.content = '<div class="infoWindowContent">' + info.title + '</div>';
+        google.maps.event.addListener(marker, 'click', function(){
+            infoWindow.setContent('<h3>' + marker.title + '</h3>');
+            infoWindow.open($scope.map, marker);
+        });
+        $scope.markers.push(marker);
+    };
+
+    var setMapOnAll = function(map) {
+        for (var i = 0; i < $scope.markers.length; i++) {
+          $scope.markers[i].setMap(map);
+        }
+    }
+    var clearMarkers = function() {
+        for (var i = 0; i < $scope.markers.length; i++) {
+          $scope.markers[i].setMap(null);
+        }
+    }
+
+    $rootScope.$on('rootScope:emit', function (event, data){
+    
+        //on emit, query the server with locations array
+        //server will send a request to Google places, and, upon
+        //receipt, should respond with and array latitudes and longitudes
+        //which match the place names.
+        $http.post('http://localhost:3000/markers', $rootScope.locations)
+            .success(function(data){
+                //console.log('success', data);
+            }).error(function(data, status){
+                console.log('Error! ' + status + ' : ' + data);
+            })
+            .then(function(res) {
+                clearMarkers();
+                console.log('responseData: ', res.data);
+                var len = res.data.length;
+                $scope.markers = [];
+                for(var i = 0; i < len; i++){
+                   // var info = res.data;
+                    var info = {
+                        name: res.data[i].name,
+                        lat: res.data[i].lat, 
+                        long: res.data[i].lng
+                    };
+                    console.log('marker info:', info);
+                    createMarker(info);
+                }
+            });
+    });
+
+    $scope.openInfoWindow = function(e, selectedMarker){
+        e.preventDefault();
+        google.maps.event.trigger(selectedMarker, 'click');
+    };
+})
+
+
---
+++ server/server.js
@@ -0,0 +1,93 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+var server = app.listen(port, function() {
+  console.log('Server listening on port ' + port);
+});
+
+app.use(bodyParser.json());
+
+//Path for serving static files
+app.use('/public', express.static(path.join(__dirname, '/../public')));
+app.use('/bower_components', express.static(path.join(__dirname + '/../bower_components')));
+
+//Load index when server is started
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname + '/../index.html'));
+});
+
+//CORS nonsense
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+//global variable which will be returned to client
+var coordArray = [];
+
+app.post('/markers', function (req, res, next) {
+  //empty coordinate array 
+  coordArray = [];
+  console.log('request body',req.body);
+  var promises = [];
+  //load an array of promises for each coordinate in the request
+  req.body.forEach(function(x){
+    promises.push(placesReq(x));
+  });
+  Promise.all(promises).then(function() {
+    //once all coordinates have been returned,
+    //send coordinate array back to client
+    setTimeout(function() {
+      console.log('results: ', coordArray);
+      res.send(coordArray);
+    }, 1000);
+  }).catch(function(reason) {
+    console.log(reason);
+    res.send(reason);
+  });
+});
+
+//google places api endpoint, place name needed
+var mapsKey = require('./config.js').key;
+var endpoint = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key='+mapsKey+
+  '&location=37.78,-122.42&radius=10000&name=';
+
+function placesReq(placeName){
+    var promise = new Promise(function(resolve, reject) {
+
+      request(endpoint+placeName, function (error, response, body) {
+        var coordinates = {};
+        if (!error && response.statusCode == 200) {
+          
+          if(JSON.parse(body).results[0] !== undefined){
+            //if lat & are found they'll be found here 
+            coordinates = JSON.parse(body).results[0].geometry.location;
+            coordinates.name = placeName;
+          } else { 
+            console.log('placename', placeName)
+            //if not, we set them to null
+            coordinates = {name: placeName, lat: null, lng: null};
+            console.log('coordinates not found', body);
+          }
+
+          console.log('latLong: ', coordinates);
+          //push set of coordinates to coordinates array
+          coordArray.push(coordinates);
+        } else{
+          console.log('error!');
+        }
+      });
+      // req.on('error', function(e) {
+      //   console.log('RETURNING ERROR: ' + e.message);
+      //   reject(e);
+      // });
+  return promise;
+  });
+}
+
